d8d0c68d9da29e4c457c8c84982c54b5788adfae
Andres7027 <80983026+Andres7027@users.noreply.github.com>
2022-06-02 22:40:57 -0500

    Add files via upload

---
+++ Parcial 2/Práctica 2/AppLogin/AppLogin/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppLogin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            String sUsuario = txtUsuario.Text;
+            String sPassword = txtPassword.Text;
+
+            if(sUsuario == "AndresATzuc001@hotmail.com" && sPassword == "@5001Fk" +
+                "f")
+            {
+                Navigation.PushAsync(new Dashboard());
+            }
+            else
+            {
+                lblResultado.Text = "El usuario o contraseña son incorrectos";
+            }
+        }
+    }
+}
